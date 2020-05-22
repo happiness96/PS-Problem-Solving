fdb1c0f9525cbea3a1f983bae0ab4afe4ad92931
forever_happiness <daewonkim.mate@gmail.com>
2020-05-23 02:05:33 +0900

    1000 A+B

--- Baekjoon_Online_Judge/NO_01000/1000.cpp
+++ Baekjoon_Online_Judge/NO_01000/1000.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 05                 | 
+ | Day 23                   | 
+ | 1000 A+B                 | 
+ ---------------------------- 
+*/
 
 int main(){
-    int a=0, b=0;
+    int a, b;
 
     cin >> a >> b;
-    cout << a + b << endl;
+    cout << a + b;
 
     return 0;
 }
