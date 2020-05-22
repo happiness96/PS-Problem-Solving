d6d8f4c35ff1ed5eb3e5ba0d78bf8c24349459f9
forever_happiness <daewonkim.mate@gmail.com>
2020-05-22 21:38:18 +0900

    1000 A+B

--- Baekjoon_Online_Judge/NO_01000/1000.c
+++ Baekjoon_Online_Judge/NO_01000/1000.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 05                 | 
+ | Day 22                   | 
+ | 1000 A+B                 | 
+ ---------------------------- 
+*/
 
-int main()
-{
+int main(){
     int a, b;
+
     scanf("%d %d", &a, &b);
     printf("%d", a + b);
-
+    
     return 0;
 }
