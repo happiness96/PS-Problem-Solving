3f6a05095adb68136b34f467aa362a34f2903717
forever_happiness <daewonkim.mate@gmail.com>
2020-05-23 20:07:12 +0900

    12871 무한 문자열

---
+++ Baekjoon_Online_Judge/NO_12871/12871.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------   --------- #
+# | Created by happiness96   | #
+# | Year 2020                | #
+# | Month 05                 | #
+# | Day 23                   | #
+# | 12871 무한 문자열          | #
+# ---------------------------- #
+
+
+def run():
+    s = r_input().rstrip()
+    t = r_input().rstrip()
+    
+    len_s = len(s)
+    len_t = len(t)
+
+    print(int(s * len_t == t * len_s))
+
+
+if __name__ == "__main__":
+    run()
