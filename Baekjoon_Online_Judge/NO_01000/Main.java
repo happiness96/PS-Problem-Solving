4e75d9a198527fe6b129f78a2d9fb705cd50bcb6
forever_happiness <daewonkim.mate@gmail.com>
2020-05-23 18:52:16 +0900

    1000 A+B

--- Baekjoon_Online_Judge/NO_01000/Main.java
+++ Baekjoon_Online_Judge/NO_01000/Main.java
@@ -1,17 +1,28 @@
 package Baekjoon_Online_Judge.NO_01000;
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 05                 | 
+ | Day 23                   | 
+ | 1000 A+B                 | 
+ ---------------------------- 
+*/
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        
+        int a, b;
 
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
 
-        // System.out.println(a + b);
-        // sc.close();
+        System.out.println(a + b);
+
+        sc.close();
     }
     
 }
