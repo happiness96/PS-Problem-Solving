e1cfcb5123edb212a90df362e6186eeea80a15db
forever_happiness <daewonkim.mate@gmail.com>
2020-05-29 09:55:11 +0900

    15963 CASIO

---
+++ Baekjoon_Online_Judge/NO_15963/15963.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 05                 | 
+ | Day 28                   | 
+ | 15963 CASIO              | 
+ ---------------------------- 
+*/
+
+int main(){
+    int N, M;
+
+    scanf("%d %d",&N, &M);
+
+    if(N == M) printf("%d", 1);
+    else printf("0");
+
+    return 0;
+}
