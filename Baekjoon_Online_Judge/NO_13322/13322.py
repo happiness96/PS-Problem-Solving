9071c10229217503ef5c3ca63af987eb2e9f0ad5
forever_happiness <daewonkim.mate@gmail.com>
2020-06-28 22:30:26 +0900

    13322 접두사 배열

---
+++ Baekjoon_Online_Judge/NO_13322/13322.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# ---------------------------- #
+# | Created by happiness96   | #
+# | Year 2020                | #
+# | Month 06                 | #
+# | Day 28                   | #
+# | 13322 접두사 배열          | #
+# ---------------------------- #
+
+
+def run():
+    for ind, _ in enumerate(r_input().rstrip()):
+        print(ind)
+
+
+if __name__ == "__main__":
+    run()
