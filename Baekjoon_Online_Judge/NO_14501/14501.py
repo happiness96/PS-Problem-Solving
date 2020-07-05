339bba4ee54fca7214ce9122224ca6897aaa9e52
forever_happiness <daewonkim.mate@gmail.com>
2020-07-05 19:11:25 +0900

    14501 퇴사

---
+++ Baekjoon_Online_Judge/NO_14501/14501.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# ---------------------------- #
+# | Created by happiness96   | #
+# | Year 2020                | #
+# | Month 07                 | #
+# | Day 05                   | #
+# | 14501 퇴사                | #
+# ---------------------------- #
+
+
+def run():
+    N = int(r_input())
+
+    dp = [0] * (N + 1)
+
+    maxi = 0
+
+    for i in range(N):
+        T, P = map(int, r_input().split())
+
+        maxi = max(maxi, dp[i])
+        aft_ind = i + T
+
+        if aft_ind <= N:
+            dp[aft_ind] = max(dp[aft_ind], maxi + P)
+    
+    print(max(maxi, dp[-1]))
+
+
+if __name__ == "__main__":
+    run()
