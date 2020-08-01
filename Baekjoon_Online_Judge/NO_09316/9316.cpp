be87db1c8397be43476fcd1467c8ab1671fb9a44
forever_happiness <daewonkim.mate@gmail.com>
2020-08-02 00:42:23 +0900

    9316 Hello Judge

---
+++ Baekjoon_Online_Judge/NO_09316/9316.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 08                 | 
+ | Day 02                   | 
+ | 9316 Hello Judge         |
+ ---------------------------- 
+*/
+
+int main(){
+    int N;
+
+    cin >> N;
+
+    for(int i = 1; i <= N; i ++) cout << "Hello World, Judge " << i << "!\n";
+    return 0;
+}
---
+++ Baekjoon_Online_Judge/NO_09316/9316.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# ---------------------------- #
+# | Created by happiness96   | #
+# | Year 2020                | #
+# | Month 08                 | #
+# | Day 02                   | #
+# | 9316 Hello Judge         | #
+# ---------------------------- #
+
+
+if __name__ == "__main__":
+    N = int(r_input())
+
+    for n in range(1, N + 1):
+        print('Hello World, Judge', n, end='!\n')
