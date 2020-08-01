f163513e788e33575721ae4d32df73b0a5481c3f
forever_happiness <daewonkim.mate@gmail.com>
2020-08-02 00:10:24 +0900

    11382 꼬마 정민

---
+++ Baekjoon_Online_Judge/NO_11382/11382.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+ ---------------------------- 
+ | Created by happiness96   | 
+ | Year 2020                | 
+ | Month 08                 | 
+ | Day 02                   | 
+ | 11382 꼬마 정민            | 
+ ---------------------------- 
+*/
+
+int main(){
+    long long A, B, C;
+
+    cin >> A >> B >> C;
+
+    cout << A + B + C;
+    return 0;
+}
---
+++ Baekjoon_Online_Judge/NO_11382/11382.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# ---------------------------- #
+# | Created by happiness96   | #
+# | Year 2020                | #
+# | Month 08                 | #
+# | Day 02                   | #
+# | 11382 꼬마 정민            | #
+# ---------------------------- #
+
+
+if __name__ == "__main__":
+    print(sum(map(int, r_input().split())))
