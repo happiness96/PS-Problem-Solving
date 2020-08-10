ba9cce5fec637233cccad5cf80538c73ec029b48
happiness96 <daewonkim.mate@gmail.com>
2020-08-10 12:04:18 +0900

    14935 FA

---
+++ Baekjoon_Online_Judge/NO_14935/14935.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 08                           | #
+# | Day 10                             | #
+# | 14935 FA                           | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    print('FA')
