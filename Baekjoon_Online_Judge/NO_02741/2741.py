77594199d8882e9fa7473ea07fae4f8582812fb6
happiness96 <daewonkim.mate@gmail.com>
2020-09-02 14:55:34 +0900

    2741 N 찍기

---
+++ Baekjoon_Online_Judge/NO_02741/2741.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 09                           | #
+# | Day 02                             | #
+# | N 찍기                              | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    N = int(r_input())
+
+    for n in range(1, N + 1):
+        print(n)
