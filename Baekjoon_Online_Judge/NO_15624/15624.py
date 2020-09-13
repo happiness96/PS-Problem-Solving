29a27e6f205289d6ef943996265bb170fd99ad22
happiness96 <daewonkim.mate@gmail.com>
2020-09-13 18:15:21 +0900

    피보나치 수 7

---
+++ Baekjoon_Online_Judge/NO_15624/15624.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 09                           | #
+# | Day 13                             | #
+# | 15624 피보나치 수 7                 | #
+# -------------------------------------- #
+
+
+def run():
+    n = int(r_input())
+    
+    a, b = 0, 1
+
+    if n == 0:
+        print(a)
+        sys.exit()
+    
+    for _ in range(n - 1):
+        a, b = b, (a + b) % 1000000007
+    
+    print(b)
+
+
+if __name__ == "__main__":
+    run()
