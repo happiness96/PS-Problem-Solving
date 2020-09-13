cb98c2ae52b3c79d4027607743f4831a2c802c70
happiness96 <daewonkim.mate@gmail.com>
2020-09-13 18:33:08 +0900

    그대로 출력하기

---
+++ Baekjoon_Online_Judge/NO_11718/11718.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 09                           | #
+# | Day 13                             | #
+# | 11718 그대로 출력하기              | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    while True:
+        tmp = r_input().rstrip()
+
+        if tmp:
+            print(tmp)
+        
+        else:
+            break
