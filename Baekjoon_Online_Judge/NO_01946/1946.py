9fd7cef4e5385125fd9d7a02ca85ddae8ad60316
happiness96 <daewonkim.mate@gmail.com>
2020-09-17 14:26:26 +0900

    1946 신입사원

--- Baekjoon_Online_Judge/NO_01946/1946.py
+++ Baekjoon_Online_Judge/NO_01946/1946.py
@@ -2,13 +2,13 @@
 import sys
 r_input = sys.stdin.readline
 
-# ---------------------------- #
-# | Created by happiness96   | #
-# | Year 2020                | #
-# | Month 07                 | #
-# | Day 15                   | #
-# | 1946 신입 사원             | #
-# ---------------------------- #
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 09                           | #
+# | Day 17                             | #
+# | 1946 신입사원                       | #
+# -------------------------------------- #
 
 
 def run():
@@ -17,13 +17,23 @@ def run():
     for _ in range(T):
         N = int(r_input())
 
-        save = [0] * (N + 1)
+        score = [0] * (N + 1)
 
         for _ in range(N):
-            A, B = map(int, r_input().split())
-            save[A] = B
-        print(save)
+            score1, score2 = map(int, r_input().split())
 
-    
+            score[score1] = score2
+
+        res = 0
+        tmp = N + 1
+
+        for ind in range(1, N + 1):
+            if score[ind] < tmp:
+                res += 1
+                tmp =score[ind]
+        
+        print(res)
+
+
 if __name__ == "__main__":
     run()
