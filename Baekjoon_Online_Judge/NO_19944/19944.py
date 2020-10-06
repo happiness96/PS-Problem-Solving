2e75974f70c52f9dd84248e77d7f75926749c0c3
forever_happiness <daewonkim.mate@gmail.com>
2020-10-06 20:23:13 +0900

    19944 뉴비의 기준은 뭘까?

---
+++ Baekjoon_Online_Judge/NO_19944/19944.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 10                           | #
+# | Day 06                             | #
+# | 19944뉴비의 기준은 뭘까?            | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    N, M = map(int, r_input().split())
+
+    print('NEWBIE!' if M < 3 else 'OLDBIE!' if M <= N else 'TLE!')
