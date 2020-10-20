ec4ab77a54f4725b91a0e536bb72b8a4c8d2069a
forever_happiness <daewonkim.mate@gmail.com>
2020-10-20 20:14:29 +0900

    16955 오목, 이길 수 있을까?

--- Baekjoon_Online_Judge/NO_16955/16955.py
+++ Baekjoon_Online_Judge/NO_16955/16955.py
@@ -2,14 +2,51 @@
 import sys
 r_input = sys.stdin.readline
 
-# ---------------------------- #
-# | Created by happiness96   | #
-# | Year 2020                | #
-# | Month 07                 | #
-# | Day 22                   | #
-# | 16955 오목, 이길 수 있을까? | #
-# ---------------------------- #
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 10                           | #
+# | Day 20                             | #
+# | 16955 오목, 이길 수 있을까?          | #
+# -------------------------------------- #
+
+
+def run():
+    board = [list(r_input().rstrip()) for _ in range(10)]
+
+    for row in range(10):
+        for col in range(10):
+            if board[row][col] == '.':
+                board[row][col] = 'X'
+
+                tmp1 = ''.join(board[row])
+                tmp2 = ''
+                tmp3 = ''
+                tmp4 = ''
+
+                for tmp_row in range(10):
+                    tmp2 += board[tmp_row][col]
+                
+                for k in range(-9, 10):
+                    tmp_row, tmp_col = row + k, col + k
+
+                    if 0 <= tmp_row < 10 and 0 <= tmp_col < 10:
+                        tmp3 += board[tmp_row][tmp_col]
+                    
+                    tmp_row, tmp_col = row - k, col + k
+
+                    if 0 <= tmp_row < 10 and 0 <= tmp_col < 10:
+                        tmp4 += board[tmp_row][tmp_col]
+                
+                for check in [tmp1, tmp2, tmp3, tmp4]:
+                    if 'XXXXX' in check:
+                        print(1)
+                        sys.exit()
+
+                board[row][col] = '.'
+
+    print(0)
 
 
 if __name__ == "__main__":
-    board = []
+    run()
