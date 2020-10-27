356b7fc4f2cbee82d72b158f515317c3467baf9c
forever_happiness <daewonkim.mate@gmail.com>
2020-10-27 21:24:47 +0900

    1402 아무래도이문제는A번난이도인것같다

---
+++ Baekjoon_Online_Judge/NO_01402/1402.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+ -------------------------------------- 
+ | Created by happiness96             | 
+ | Year 2020                          | 
+ | Month 10                           | 
+ | Day 27                             | 
+ | 1402 아무래도이문제는A번난이도인것같다 | 
+ -------------------------------------- 
+*/
+
+int main(){
+    int T;
+
+    cin >> T;
+
+    for(int i = 0; i < T; i ++) cout << "yes" << '\n';
+    return 0;
+}
