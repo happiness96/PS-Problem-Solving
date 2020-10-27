168662726365c12b994a1763da4016653308475e
forever_happiness <daewonkim.mate@gmail.com>
2020-10-27 21:06:57 +0900

    1402 아무래도이문제는A번난이도인것같다

---
+++ Baekjoon_Online_Judge/NO_01402/1402.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 10                           | #
+# | Day 27                             | #
+# | 1402 아무래도이문제는A번난이도인것같다 | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    T = int(r_input())
+
+    print(*['yes'] * T, sep='\n')
