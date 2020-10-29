c5af3b95b6cf4a80ff6aa83587971448598f4281
forever_happiness <daewonkim.mate@gmail.com>
2020-10-29 21:08:11 +0900

    15501 부당한 퍼즐

--- Baekjoon_Online_Judge/NO_15501/15501.py
+++ Baekjoon_Online_Judge/NO_15501/15501.py
@@ -14,8 +14,8 @@ r_input = sys.stdin.readline
 if __name__ == "__main__":
     n = int(r_input())
 
-    first = list(map(int, r_input().rstrip().split()))
-    second = list(map(int, r_input().rstrip().split()))
+    first = list(map(int, r_input().split()))
+    second = list(map(int, r_input().split()))
     
     ind = second.index(first[0])
     
