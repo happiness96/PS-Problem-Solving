4e26863f2e41e1182efcf633abc84b93939a8260
forever_happiness <daewonkim.mate@gmail.com>
2020-10-31 14:17:29 +0900

    18229 내가 살게, 아냐 내가 살게

---
+++ Baekjoon_Online_Judge/NO_18229/18229.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 10                           | #
+# | Day 31                             | #
+# | 18229 내가 살게, 아냐 내가 살게     | #
+# -------------------------------------- #
+
+
+def run():
+    N, M, K = map(int, r_input().split())
+
+    who_charge = 0
+    min_cnt = sys.maxsize
+
+    for no in range(1, N + 1):
+        A = list(map(int, r_input().split()))
+
+        tot = 0
+
+        for ind, val in enumerate(A):
+            tot += val
+
+            if tot >= K:
+                if ind < min_cnt:
+                    min_cnt = ind
+                    who_charge = no
+    
+    print(who_charge, min_cnt + 1)
+
+
+if __name__ == "__main__":
+    run()
