1197899bc474f5be862865bc7e372ab985e2ee54
forever_happiness <daewonkim.mate@gmail.com>
2020-11-04 14:41:34 +0900

    11054 가장 긴 바이토닉 부분 수열

--- Baekjoon_Online_Judge/NO_11054/11054.py
+++ Baekjoon_Online_Judge/NO_11054/11054.py
@@ -2,38 +2,38 @@
 import sys
 r_input = sys.stdin.readline
 
-# ---------------------------- #
-# | Created by happiness96   | #
-# | Year 2020                | #
-# | Month 05                 | #
-# | Day 28                   | #
-# | 11054 가장 긴 바이토닉 부분 수열| #
-# ---------------------------- #
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 11                           | #
+# | Day 04                             | #
+# | 11054 가장 긴 바이토닉 부분 수열     | #
+# -------------------------------------- #
 
-
-def run():
-    N = int(r_input())
-
-    A = list(map(int, r_input().split()))
-
-    result = 0
-    maxi = max(A) + 1
-    for i in range(N):
-        left = [0] * maxi
-        right = [0] * maxi
-        
-        for ind in range(i):
-            left[A[ind]] = max(left[A[ind]], max(left[:A[ind]]) + 1)
-        
-        for ind in range(N - 1, i, -1):
-            right[A[ind]] = max(right[A[ind]], max(right[:A[ind]]) + 1)
-        print(i)
-        print(left)
-        print(right)
-        result = max(result, max(left[:A[ind] + 1]) + max(right[:A[ind] + 1]) + 1)
-    
-    print(result)
-    
 
 if __name__ == "__main__":
-    run()
+    N = int(r_input())
+    A = list(map(int, r_input().split()))
+
+    increasing = [0] * 1001
+    increasing_dp = [0] * N
+    decreasing = [0] * 1001
+    decreasing_dp = [0] * N
+
+    for ind, val in enumerate(A):
+        tmp = max(increasing[: val]) + 1
+        increasing[val] = tmp
+        increasing_dp[ind] = tmp
+
+    for ind in range(N - 1, -1, -1):
+        val = A[ind]
+        tmp = max(decreasing[: val]) + 1
+        decreasing[val] = tmp
+        decreasing_dp[ind] = tmp
+    
+    maxi = 0
+
+    for ind in range(N):
+        maxi = max(maxi, increasing_dp[ind] + decreasing_dp[ind])
+
+    print(maxi - 1)
