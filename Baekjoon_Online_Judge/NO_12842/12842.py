ae1b5153f5162bad8aca533a3a97077ad71b0076
forever_happiness <daewonkim.mate@gmail.com>
2020-11-13 00:39:52 +0900

    12842 튀김 소보루

---
+++ Baekjoon_Online_Judge/NO_12842/12842.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+import sys
+from heapq import *
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2020                          | #
+# | Month 11                           | #
+# | Day 12                             | #
+# | 12842 튀김 소보루                   | #
+# -------------------------------------- #
+
+
+def run():
+    n, s = map(int, r_input().split())
+    rem = n - s
+    
+    m = int(r_input())
+    t = []
+    save = [[] for _ in range(100000)]
+
+    for i in range(m):
+        val = int(r_input())
+        t.append(val)
+        save[0].append(i)
+    
+    for ind, val_list in enumerate(save):
+        for val in sorted(val_list):
+            rem -= 1
+
+            if not rem:
+                print(val + 1)
+                sys.exit()
+            
+            save[ind + t[val]].append(val)
+
+
+if __name__ == "__main__":
+    run()
