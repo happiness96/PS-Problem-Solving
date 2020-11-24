674c50eda6ded7a930537dfca22c085b778e8779
forever_happiness <daewonkim.mate@gmail.com>
2020-11-24 20:08:05 +0900

    18795 이동하기

---
+++ Baekjoon_Online_Judge/NO_18795/18795.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+/*
+ -------------------------------------- 
+ | Created by happiness96             | 
+ | Year 2020                          | 
+ | Month 11                           | 
+ | Day 24                             | 
+ | 18795 이동하기                     | 
+ -------------------------------------- 
+*/
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long N, M, total = 0;
+
+    cin >> N >> M;
+
+    for(int i = 0; i < N + M; i ++){
+        long long k;
+
+        cin >> k;
+
+        total += k;
+    }
+
+    cout << total;
+    return 0;
+}
